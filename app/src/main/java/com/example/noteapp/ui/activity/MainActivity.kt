176275163f363139ac40830b657424bd1907ef23
rinkkooo <bekturtelmanov04@gmail.com>
2024-05-16 14:11:08 +0600

    On Board, Text Skip, Tab Layout, shared preference

--- app/src/main/java/com/example/noteapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/ui/activity/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.noteapp.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import com.example.noteapp.R
+import com.example.noteapp.databinding.ActivityMainBinding
+import com.example.noteapp.utils.PreferenceHelper
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var preferenceHelper: PreferenceHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        preferenceHelper = PreferenceHelper()
+        preferenceHelper.unit(this)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        if (!preferenceHelper.isOnBoardShow) {
+            navController.navigate(R.id.noteFragment)
+            preferenceHelper.isOnBoardShow = true
+        }else if(preferenceHelper.isOnBoardShow){
+            navController.navigate(R.id.onBoardFragment)
+        }
     }
+
 }
